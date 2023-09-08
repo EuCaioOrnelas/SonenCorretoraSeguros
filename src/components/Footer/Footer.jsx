6240cee5ee473440719d0791eb215798054ffa20
Caio Ornelas <caioornelasoficial@gmail.com>
2023-09-08 07:47:58 -0300

    responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,57 +22,59 @@ function Footer() {
               </p>
             </div>
           </div>
-          <div className="acesso_rapido">
-            <h3>ACESSO RÁPIDO</h3>
-            <ul>
-              <li>
-                <a href="https://api.whatsapp.com/send?phone=5544998183338&text=Ol%C3%A1!!!%20gostaria%20de%20tirar%20uma%20d%C3%BAvida...">
-                  Contato
-                </a>
-              </li>
-              <li>
-                <a href="https://forms.gle/Pd9k2XH4j86Q6qy26">
-                  Fazer Cotação Online
-                </a>
-              </li>
-              <li>
-                <a href="#about">Sobre Nós</a>
-              </li>
-              <li>
-                <a href="#services">Seguros</a>
-              </li>
-              <li>
-                <a href="#localizacao">Localização</a>
-              </li>
-            </ul>
-          </div>
-          <div className="contatos_rapidos">
-            <h3>CONTATOS RÁPIDO</h3>
-            <ul>
-              <li>
-                <a href="mailto:Sonencorretora@hotmail.com">Enviar E-mail</a>
-              </li>
-              <li>
-                <a href="https://api.whatsapp.com/send?phone=5544999738747">
-                  WhatsApp Corretor
-                </a>
-              </li>
-              <li>
-                <a href="https://api.whatsapp.com/send?phone=5544999738747">
-                  Ligação Corretor
-                </a>
-              </li>
-              <li>
-                <a href="https://api.whatsapp.com/send?phone=5544999738747&text=Ol%C3%A1,%20estou%20precisando%20de%20ajuda!">
-                  Sinistro
-                </a>
-              </li>
-              <li>
-                <a href="https://api.whatsapp.com/send?phone=5544999738747&text=Ol%C3%A1,%20preciso%20acionar%20um%20guincho!">
-                  Solicitar Guincho
-                </a>
-              </li>
-            </ul>
+          <div className="acessos">
+            <div className="acesso_rapido">
+              <h3>ACESSO RÁPIDO</h3>
+              <ul>
+                <li>
+                  <a href="https://api.whatsapp.com/send?phone=5544998183338&text=Ol%C3%A1!!!%20gostaria%20de%20tirar%20uma%20d%C3%BAvida...">
+                    Contato
+                  </a>
+                </li>
+                <li>
+                  <a href="https://forms.gle/Pd9k2XH4j86Q6qy26">
+                    Fazer Cotação Online
+                  </a>
+                </li>
+                <li>
+                  <a href="#about">Sobre Nós</a>
+                </li>
+                <li>
+                  <a href="#services">Seguros</a>
+                </li>
+                <li>
+                  <a href="#localizacao">Localização</a>
+                </li>
+              </ul>
+            </div>
+            <div className="contatos_rapidos">
+              <h3>CONTATOS RÁPIDO</h3>
+              <ul>
+                <li>
+                  <a href="mailto:Sonencorretora@hotmail.com">Enviar E-mail</a>
+                </li>
+                <li>
+                  <a href="https://api.whatsapp.com/send?phone=5544999738747">
+                    WhatsApp Corretor
+                  </a>
+                </li>
+                <li>
+                  <a href="https://api.whatsapp.com/send?phone=5544999738747">
+                    Ligação Corretor
+                  </a>
+                </li>
+                <li>
+                  <a href="https://api.whatsapp.com/send?phone=5544999738747&text=Ol%C3%A1,%20estou%20precisando%20de%20ajuda!">
+                    Sinistro
+                  </a>
+                </li>
+                <li>
+                  <a href="https://api.whatsapp.com/send?phone=5544999738747&text=Ol%C3%A1,%20preciso%20acionar%20um%20guincho!">
+                    Solicitar Guincho
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="rodape_footer">
